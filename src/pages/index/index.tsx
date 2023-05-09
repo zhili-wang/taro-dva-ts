760fa96f1dd5ae2dda65c5b07f69f94ee7c49b9f
wangzl <1498129827@qq.com>
2023-05-09 11:07:26 +0800

    fix: 代码优化

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -36,23 +36,11 @@ export default function Index() {
   // 对应 onHide
   useDidHide(() => { })
 
-  // Taro 对所有小程序页面生命周期都实现了对应的自定义 React Hooks 进行支持
-  // 详情可查阅：【Hooks】
   usePullDownRefresh(() => { })
 
   return (
     <View>
       <Text>Hello world!</Text>
-      {/* <AtTabBar
-        fixed
-        tabList={[
-          { title: '首页', iconType: 'home' },
-          { title: '我的', iconType: 'user' }
-        ]}
-        current={0}
-        onClick={function (index: number, event: CommonEvent): void {
-          throw new Error('Function not implemented.')
-        }} /> */}
     </View>
   )
 }
